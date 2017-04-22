e040c5079da24a8fa65ac4e9a3f94aa19f79ca9f
ningkai <ningkaiqunar@gmail.com>
2017-04-22 20:00:24 +0800

    add bootstrap-sprockets

--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,12 @@ Rails.application.routes.draw do
       end
       resources :resumes
 
-      
+
   end
 end
 resources :jobs do
   resources :resumes
 end
-  root 'jobs#index'
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
